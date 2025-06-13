decc61bff6c085e13c9b491a98dda143aba3ffdd
DonaldFaQ <qfg@inbox.lv>
2025-06-13 19:39:41 +0200

    Example file for multiindex use.

---
+++ multiindex_example.cmd
@@ -0,0 +1,16 @@
+@echo off & setlocal
+setlocal EnableDelayedExpansion
+set "FSACpath=C:\FSAC"
+set "file=%~1"
+if "!file!"=="" (
+	echo Batch script for multi index encoding. Please drag and drop input file
+	echo or type ^<%~n0 "MyFile.mkv"^>
+) else (
+	:: Start all commandos her with call!
+	call "!FSACpath!\FSAC.cmd" "!file!" --Index-2
+	call "!FSACpath!\FSAC.cmd" "!file!" --Index-3
+)
+echo.
+setlocal DisableDelayedExpansion
+timeout 30
+exit
